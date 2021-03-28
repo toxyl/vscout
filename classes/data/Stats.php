7854a4030cd072aad01cdba3306234a332cdab4e
toxyl <tox@novasonica.com>
2021-03-28 12:38:20 +0200

    - removed Total column from stats table (already in config part of dashboard)

--- classes/data/Stats.php
+++ classes/data/Stats.php
@@ -13,13 +13,6 @@ class Stats
                 'dead'       => $stats[2] ?? 0,
             ], 
             'status_codes' => [
-                'all'         => [
-                    '1xx'        => $stats[3] ?? 0,
-                    '2xx'        => $stats[4] ?? 0,
-                    '3xx'        => $stats[5] ?? 0,
-                    '4xx'        => $stats[6] ?? 0,
-                    '5xx'        => $stats[7] ?? 0,
-                ],
                 '1d'         => [
                     '1xx'        => $stats[8] ?? 0,
                     '2xx'        => $stats[9] ?? 0,
@@ -93,11 +86,10 @@ class Stats
         return ' ' . self::format_row($data);
     }
 
-    static private function header(&$out, $lbl, $lbltall = '', $lblt5m = '', $lblt15m = '', $lblt1h = '', $lblt1d = '', $lblspeed = '', $lblspeedday = '')
+    static private function header(&$out, $lbl, $lblt5m = '', $lblt15m = '', $lblt1h = '', $lblt1d = '', $lblspeed = '', $lblspeedday = '')
     {
         $out[] = self::header_format([
                     ["%12s", $lbl],
-                    ["%10s", $lbltall],
                     ["%10s", $lblt5m],
                     ["%10s", $lblt15m],
                     ["%10s", $lblt1h],
@@ -116,10 +108,9 @@ class Stats
                 ]);
     }
 
-    static private function line_status_code(&$out, $lbl, $field, $tall, $t5m, $t15m, $t1h, $t1d)
+    static private function line_status_code(&$out, $lbl, $field, $t5m, $t15m, $t1h, $t1d)
     {
         $v = [ 
-            (is_array($tall) ? $tall[$field] : $tall), 
             (is_array($t5m)  ? $t5m[$field]  : $t5m), 
             (is_array($t15m) ? $t15m[$field] : $t15m), 
             (is_array($t1h)  ? $t1h[$field]  : $t1h), 
@@ -127,16 +118,15 @@ class Stats
             (is_array($t1h)  ? $t1h[$field]  : $t1h) / 60, 
             (is_array($t1h)  ? $t1h[$field]  : $t1h) * 24 
         ];
-        $delta = $v[4] - $v[6];
+        $delta = $v[3] - $v[5];
         $out[] = self::line_format([
-                    ["%12s",           $lbl], 
-                    ["%10d",           $v[0]], 
-                    ["%10d",           $v[1]],
+                    ["%12s",           $lbl],  
+                    ["%10d",           $v[0]],
+                    ["%10d",           $v[1]], 
                     ["%10d",           $v[2]], 
                     ["%10d",           $v[3]], 
-                    ["%10d",           $v[4]], 
-                    ["%10.2f / min",   $v[5]],
-                    ["%10d / day",     $v[6]],
+                    ["%10.2f / min",   $v[4]],
+                    ["%10d / day",     $v[5]],
                     ["%3s",             $delta > 0 ? '-' : ($delta == 0 ? ' ' : '+')]
                 ]);
         return $v;
@@ -150,7 +140,6 @@ class Stats
         $s15m = $s['status_codes']['15m'];
         $s1h  = $s['status_codes']['1h'];
         $s1d  = $s['status_codes']['1d'];
-        $sall = $s['status_codes']['all'];
 
         $response[] = Router::is_cli() ? "\n" : "<table>";
 
@@ -170,22 +159,21 @@ class Stats
 
         $response[] = Router::is_cli() ? "\n" : "</table><br><table>";
 
-        self::header($response, 'Status Codes', 'Total', '5m', '15m', '1h', '1d', 'average speed / min (based on 1h)', 'average speed / day (based on 1h)');
+        self::header($response, 'Status Codes', '5m', '15m', '1h', '1d', 'average speed / min (based on 1h)', 'average speed / day (based on 1h)');
 
-        $v1xx = self::line_status_code($response, '1xx',   '1xx', $sall, $s5m, $s15m, $s1h, $s1d);
-        $v2xx = self::line_status_code($response, '2xx',   '2xx', $sall, $s5m, $s15m, $s1h, $s1d);
-        $v3xx = self::line_status_code($response, '3xx',   '3xx', $sall, $s5m, $s15m, $s1h, $s1d);
-        $v4xx = self::line_status_code($response, '4xx',   '4xx', $sall, $s5m, $s15m, $s1h, $s1d);
-        $v5xx = self::line_status_code($response, '5xx',   '5xx', $sall, $s5m, $s15m, $s1h, $s1d);
+        $v1xx = self::line_status_code($response, '1xx',   '1xx', $s5m, $s15m, $s1h, $s1d);
+        $v2xx = self::line_status_code($response, '2xx',   '2xx', $s5m, $s15m, $s1h, $s1d);
+        $v3xx = self::line_status_code($response, '3xx',   '3xx', $s5m, $s15m, $s1h, $s1d);
+        $v4xx = self::line_status_code($response, '4xx',   '4xx', $s5m, $s15m, $s1h, $s1d);
+        $v5xx = self::line_status_code($response, '5xx',   '5xx', $s5m, $s15m, $s1h, $s1d);
 
         $vAll = [
-            'total' =>  $v1xx[0] + $v2xx[0] + $v3xx[0] + $v4xx[0] + $v5xx[0],
-            '5m'    =>  $v1xx[1] + $v2xx[1] + $v3xx[1] + $v4xx[1] + $v5xx[1],
-            '15m'   =>  $v1xx[2] + $v2xx[2] + $v3xx[2] + $v4xx[2] + $v5xx[2],
-            '1h'    =>  $v1xx[3] + $v2xx[3] + $v3xx[3] + $v4xx[3] + $v5xx[3],
-            '1d'    =>  $v1xx[4] + $v2xx[4] + $v3xx[4] + $v4xx[4] + $v5xx[4],
+            '5m'    =>  $v1xx[0] + $v2xx[0] + $v3xx[0] + $v4xx[0] + $v5xx[0],
+            '15m'   =>  $v1xx[1] + $v2xx[1] + $v3xx[1] + $v4xx[1] + $v5xx[1],
+            '1h'    =>  $v1xx[2] + $v2xx[2] + $v3xx[2] + $v4xx[2] + $v5xx[2],
+            '1d'    =>  $v1xx[3] + $v2xx[3] + $v3xx[3] + $v4xx[3] + $v5xx[3],
         ];
-        self::line_status_code($response, 'total', '', $vAll['total'], $vAll['5m'], $vAll['15m'], $vAll['1h'], $vAll['1d']);
+        self::line_status_code($response, 'total', '', $vAll['5m'], $vAll['15m'], $vAll['1h'], $vAll['1d']);
 
         $response[] = Router::is_cli() ? "\n" : "</table>";
 
